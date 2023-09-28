6cd6f6f6c5ff5499acb1db9d45028eb3d2637ece
Pedro Davi <pedro.henrique04896@gmail.com>
2023-09-28 10:52:10 -0300

    commit

--- src/modelo/produto.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Produto = /** @class */ (function () {
-    function Produto() {
-    }
-    return Produto;
-}());
-exports.default = Produto;
